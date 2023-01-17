f2a6d9d861d933e38044129cf7d4982f50f36957
Gülsüm KAYACAN <54817521+gulsumaslan@users.noreply.github.com>
2023-01-17 22:44:08 +0300

    Update Program.cs

--- Example10/Program.cs
+++ Example10/Program.cs
@@ -6,30 +6,30 @@ namespace Example10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("A Değerini Girin ");
+            Console.WriteLine("Enter A Value ");
             int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("B Değerini Girin ");
+            Console.WriteLine("Enter B Value ");
             int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("C Değerini Girin ");
+            Console.WriteLine("Enter C Value ");
             int c = Convert.ToInt32(Console.ReadLine());
             double d = (b ^2) - (4 * a * c); //kök bulma
             if (d > 0)
             {
                 double x1 = ((-b - Math.Sqrt(d)) / (2 * a));
                 double x2 = ((-b + Math.Sqrt(d)) / (2 * a));
-                Console.WriteLine("Denklemin İki Kökü Vardır ");
+                Console.WriteLine("Equation Has Two Roots ");
                 Console.WriteLine(x1);
                 Console.WriteLine( x2);
             }
             else if (d == 0)
             {
                 double x = -b / (2 * a);
-                Console.WriteLine("Denklemin Bir Kökü Vardır ");
+                Console.WriteLine("The Equation Has a Root ");
                 Console.WriteLine( x);
             }
             else
             {
-                Console.WriteLine("kök yoktur");
+                Console.WriteLine(no root");
             }
         }
     }
