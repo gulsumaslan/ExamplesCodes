d25d7271c00c2144759844e98b41ba5f5dfed3d0
Gülsüm KAYACAN <54817521+gulsumaslan@users.noreply.github.com>
2023-01-17 22:47:15 +0300

    Update Program.cs

--- Example11/Program.cs
+++ Example11/Program.cs
@@ -6,22 +6,22 @@ namespace Example11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bir ürün alış fiyatını giriniz");
-            double alis = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a product purchase price");
+            double buying = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("bir ürün vergi fiyatını giriniz");
-            double vergi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a product tax price");
+            double tax = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("bir ürün kar fiyatını giriniz");
-            double kar = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a product profit pricez");
+            double snow = Convert.ToDouble(Console.ReadLine());
 
 
 
-            double satis = alis + vergi * 0.5 + kar * 1.8;
+            double tax = buying + snow * 0.5 + snow * 1.8;
 
-            Console.WriteLine(satis);
-            //Console.WriteLine("bir ürün alış fiyatını giriniz");
-            //double satis = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(tax);
+            //Console.WriteLine("enter a product purchase price");
+            //double tax = Convert.ToDouble(Console.ReadLine());
 
         }
     }
