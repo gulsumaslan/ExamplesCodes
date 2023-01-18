d434ef814a83f47a2c4d9a6212177ec64af37d7c
Gülsüm KAYACAN <54817521+gulsumaslan@users.noreply.github.com>
2023-01-18 08:20:35 +0300

    Update Program.cs

--- Example7/Program.cs
+++ Example7/Program.cs
@@ -6,26 +6,26 @@ namespace Example7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("birinci sayıyı giriniz");
-            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("ikinci sayıyı giriniz");
-            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("üçüncü sayıyı giriniz");
-            int sayi3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the third number");
+            int number3 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("dördüncü sayıyı giriniz");
-            int sayi4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the fourth number");
+            int number4 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("beşincisayıyı giriniz");
-            int sayi5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the fifth number");
+            int number5 = Convert.ToInt32(Console.ReadLine());
 
-            int toplam = sayi1 + sayi2 + sayi3 + sayi4 + sayi5;
-            double ortalama = (sayi1 + sayi2 + sayi3 + sayi4 + sayi5) / 5;
+            int total= number1 + number2 + number3 + number4 + number5;
+            double average = (number1 + number2 + number3 + number4 + number5) / 5;
 
-            Console.WriteLine("toplam:"+toplam);
-            Console.WriteLine("Ortalama:"+ortalama);
+            Console.WriteLine("total:"+total);
+            Console.WriteLine("Average:"+average);
 
 
         }
